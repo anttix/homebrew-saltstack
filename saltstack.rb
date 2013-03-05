988807e2395e7e0d2eaf77197d189f16e762a38a
Antti Andreimann <anttix@users.sourceforge.net>
2013-03-05 11:59:32 -0800

    Automatically install dependencies if --with-deps option is passed

--- saltstack.rb
+++ saltstack.rb
@@ -5,15 +5,20 @@ class Saltstack < Formula
   url 'https://github.com/downloads/saltstack/salt/salt-0.10.5.tar.gz'
   sha1 '42796c7299e0000c250af2b3164fa77ef4f5e460'
 
+  option 'with-deps', "Install python dependencies automatically"
+
   depends_on 'swig' => :build
   depends_on 'python'
   depends_on 'zeromq'
-  depends_on 'jinja2' => :python
-  depends_on 'M2Crypto' => :python
-  depends_on LanguageModuleDependency.new :python, 'PyCrypto', 'Crypto'
-  depends_on LanguageModuleDependency.new :python, 'pyzmq', 'zmq'
-  depends_on LanguageModuleDependency.new :python, 'PyYAML', 'yaml'
-  depends_on LanguageModuleDependency.new :python, 'msgpack-python', 'msgpack'
+
+  if not build.include? 'with-deps'
+    depends_on 'jinja2' => :python
+    depends_on 'M2Crypto' => :python
+    depends_on LanguageModuleDependency.new :python, 'PyCrypto', 'Crypto'
+    depends_on LanguageModuleDependency.new :python, 'pyzmq', 'zmq'
+    depends_on LanguageModuleDependency.new :python, 'PyYAML', 'yaml'
+    depends_on LanguageModuleDependency.new :python, 'msgpack-python', 'msgpack'
+  end
 
   def patches
     # set salt config dir to live under Homebrew's etc dir
@@ -21,6 +26,10 @@ class Saltstack < Formula
   end
 
   def install
+    if build.include? 'with-deps'
+      system "pip", "install", "-r", "requirements.txt"
+    end
+
     # In order to install into the Cellar, the dir must exist and be in the PYTHONPATH.
     temp_site_packages = lib/which_python/'site-packages'
     mkdir_p temp_site_packages
